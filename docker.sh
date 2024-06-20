c30978f3d0ca091bba15be81df2fdbf5b34fdfb3
Yen-Chen Kuo <yenchenkuo9331@gmail.com>
2024-06-21 00:53:46 +0800

    chore: rename docker-compose file

--- docker.sh
+++ docker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [[ $1 == dev ]]; then
-    docker-compose -f docker-compose.yaml up
+    docker-compose -f docker-compose.local.yaml up
 elif [[ $1 == dev-rebuild ]]; then
-    docker-compose -f docker-compose.yaml up --build app
+    docker-compose -f docker-compose.local.yaml up --build app
 elif [[ $1 == clear ]]; then
-    docker-compose -f docker-compose.yaml down -v
+    docker-compose -f docker-compose.local.yaml down -v
 elif [[ $1 == migrate ]]; then
-    docker-compose -f docker-compose.yaml run --rm app alembic revision --autogenerate -m "$2"
+    docker-compose -f docker-compose.local.yaml run --rm app alembic revision --autogenerate -m "$2"
 elif [[ $1 == upgrade ]]; then
-    docker-compose -f docker-compose.yaml run --rm app alembic -x data=true upgrade  $2
+    docker-compose -f docker-compose.local.yaml run --rm app alembic -x data=true upgrade  $2
 elif [[ $1 == downgrade ]]; then
-    docker-compose -f docker-compose.yaml run --rm app alembic -x data=true downgrade $2
+    docker-compose -f docker-compose.local.yaml run --rm app alembic -x data=true downgrade $2
 elif [[ $1 == test ]]; then
     docker-compose -f docker-compose.test.yaml down -v
     docker-compose -f docker-compose.test.yaml up --exit-code-from app-test
